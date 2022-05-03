b93d368cc6ec13bfa0f3a6e2d80bf13da09c85c3
Maksym-Fedir Zvarych <maksym.zvarych@intellias.com>
2022-05-03 16:36:28 +0300

    first commit

---
+++ src/database/index.ts
@@ -0,0 +1,44 @@
+import { Connection, createConnection, SimpleConsoleLogger,useContainer  } from "typeorm";
+import { Container } from 'typeorm-typedi-extensions';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config({});
+class Database {
+
+  public connection: Connection;
+
+  constructor() {
+    this.connectToDB();
+  }
+
+  private connectToDB(): void {
+    useContainer(Container);
+    createConnection({
+      type: envString("mysql", "sqlite"),
+      host: envString(process.env.DATABASE_HOST!, ""),
+      port: envString(Number(process.env.DATABASE_PORT!), 0),
+      username: envString(process.env.DATABASE_USERNAME!, ""),
+      password: envString(process.env.DATABASE_PASSWORD!, ""),
+      database: envString(process.env.DATABASE_NAME!, "./db.sqlite"),
+      entities: [
+        __dirname + "/entity/*.ts",
+        __dirname + "/entity/*.js"
+      ],
+      synchronize: true,
+      logging: false
+    }).then(_con => {
+      this.connection = _con;
+      console.log("Connected to db!!");
+    }).catch(console.error)
+  }
+
+}
+
+
+function envString<T>(prodString: T, devString: T): T {
+  return process.env.NODE_ENV === 'production' ? prodString : devString
+}
+
+export const db = new Database();
